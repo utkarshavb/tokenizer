d8a8176799baffdbddb677977446b2ebaa466283
utkarshavb <utkarsh.avb.singh@gmail.com>
2026-02-21 06:33:32 +0000

    make test coverage for pre-tokenization more complete

--- tests/test_pretokenization.py
+++ tests/test_pretokenization.py
@@ -1,4 +1,4 @@
-import regex as re
+import regex as re, io
 from collections import Counter
 from tokenizer.pre_tokenization import refine_boundaries, pre_tokenize
 
@@ -22,3 +22,30 @@ def test_serial_parallel_implementation_equivalence():
     pre_tokens = pre_tokenize(path, PAT, special_tokens, EOT.encode())
 
     assert reference_pre_tokens == pre_tokens
+
+def test_empty_file():
+    path = "tests/files/empty.txt"
+    assert pre_tokenize(path, PAT, [], EOT.encode()) == Counter()
+
+def test_chunk_boundaries():
+    split_special_token = EOT.encode()
+    tlen = len(split_special_token)
+
+    data = (
+        b"h"*100 + split_special_token + b"!"*1000 +
+        split_special_token + split_special_token +
+        b"end"*100 + b"of"*1000
+    )
+    f = io.BytesIO(data)
+    file_sz = len(data)
+    bad_boundaries = [0] + [99, 105, 1100+2*tlen+3] + [file_sz]
+    boundaries = refine_boundaries(f, file_sz, bad_boundaries, split_special_token)
+
+    assert boundaries[0] == 0
+    assert boundaries[-1] == file_sz
+    assert boundaries == sorted(boundaries)
+    assert len(boundaries) == len(set(boundaries))
+
+    for i in range(1, len(boundaries)-1):
+        b = boundaries[i]
+        assert data[b:b+tlen] == split_special_token
