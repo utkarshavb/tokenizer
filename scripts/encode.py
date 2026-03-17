4df00681b66bfb69d3457a03ed6e49ced38d4727
utkarshavb <utkarsh.avb.singh@gmail.com>
2026-03-17 12:42:49 +0000

    infer data type directly for output array

--- scripts/encode.py
+++ scripts/encode.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     parser.add_argument("--tokenizer", type=str, required=True, help="path to a tokenizer.json file")
     parser.add_argument("--use-tiktoken", action="store_true", help="encodes with tiktoken. NOTE: requires loading the full file in memory")
     parser.add_argument("--special-tokens", type=str, nargs="*", default=[], help="tokens to treat as special while tokenizing")
-    parser.add_argument("--dtype", type=str, default="uint16", help="data type of the output array")
     args = parser.parse_args()
 
     if args.use_tiktoken:
@@ -18,10 +17,13 @@ if __name__ == "__main__":
         with open(args.text, "r", encoding="utf-8") as f:
             text = f.read()
         encoded = tokenizer.encode(text, allowed_special="all")
+        vocab_size = tokenizer.n_vocab
     else:
         tokenizer = Tokenizer.from_files(args.tokenizer, extra_specials=args.special_tokens)
         with open(args.text, "r", encoding="utf-8") as f:
             encoded = list(tokenizer.encode_iterable(f))
+        vocab_size = len(tokenizer.vocab)
 
-    encoded_arr = np.array(encoded, dtype=args.dtype)
+    dtype = np.uint16 if vocab_size<=65_535 else np.uint32
+    encoded_arr = np.array(encoded, dtype=dtype)
     np.save(args.out, encoded_arr)
